82d8fd12ca1d92f8c39c2504ace881e9b8a3bcd0
Chamod Nimz <chamodnimsaradx@gmail.com>
2018-11-26 23:43:52 +0530

    initial commit of web scrapper for N!8^^

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+var bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+
+
+const port = 3000;
+
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var request = require('request');
+
+app.get('/results/:id',function(req, res){
+	
+	var array = [];
+	var isTrue = true;
+	var result = {
+
+		subjects: [],
+		results:[]
+	};
+
+	request('https://www.nibm.lk/students/exams/results?q='+req.params.id,function(err,resp,body){
+		if(!err){
+
+			const $ = cheerio.load(body);
+
+			$('tbody tr td ').each(function(i, el){
+				const item = $(el).text();
+				if(i%2==1){
+
+					isTrue=!isTrue;
+					if(!isTrue){
+						result.subjects.push(item);
+					}else{
+						result.results.push(item);
+						
+					}
+
+				}
+
+			});
+
+		}else{
+
+			throw err;
+		}
+		res.send(result);
+	});
+	
+});
+
+
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
+
+process.on('uncaughtException',function(error){
+	console.log(error);
+});
